fe6a70435fa4d9cd610fa9c385f67b2d5914a672
Yuliia <yuliiakryshchenko@gmail.com>
2023-11-22 11:15:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Final_work
+
+### node version 
+*v21.1.0*
+
+### npm version
+*10.2.0*
+
+## Tools
+
+- Gulp
+- SCSS
---
+++ app/js/main.js
@@ -0,0 +1,47 @@
+(() => {
+	const openMenu = document.querySelector('.header__box-burger')
+	const closeMenu = document.querySelector('.header__nav-close')
+	const headerNavigation = document.querySelector('.header__nav')
+
+	openMenu.addEventListener('click', (event) => {
+		headerNavigation.classList.add('active')
+	})
+
+	closeMenu.addEventListener('click', (event) => {
+		headerNavigation.classList.remove('active')
+	})
+})();
+
+(() => {
+	const tabsItems = document.querySelectorAll('.tabs__links-item')
+	const tabsContents = document.querySelectorAll('.tabs__contents-item')
+
+	tabsItems.forEach((tab, index) => {
+		tab.addEventListener('click', (event) => {
+			tabsContents.forEach((content) => {
+				content.classList.remove('active')
+			})
+			tabsItems.forEach((tab) => {
+				tab.classList.remove('active')
+			})
+
+			tabsItems[index].classList.add('active')
+			tabsContents[index].classList.add('active')
+		})
+	})
+})();
+
+const teamSwiper = document.querySelector ('.team__swiper')
+
+if(teamSwiper) {
+	const swiper = new Swiper('.team__swiper', {
+	loop: true,
+	spaceBetween: 40,
+	grabCursor:  true,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+});
+}
+
